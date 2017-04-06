acd88ed250befd2d2a0b5c575248c2a1da65cf2c
Kaitlyn Mohr <kpmohr@gmail.com>
2017-04-06 15:39:58 -0400

    lots o stuff

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,7 +7,7 @@ $ ( () => {
   const dsiCall = () => {
     // beginning of ajax post, using data from user
     $.ajax({
-      url: 'localhost:4000',
+      url: 'localhost:4000/results',
       type: 'GET',
       success: data => {
         console.log('data: --------------');
@@ -25,192 +25,59 @@ $ ( () => {
     });
 
 
-  // Step 3. Create a data object
+
+
+
+//Data info for graph
   var data = {
-      labels: ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"],
+      labels: ["Manhattan", "The Bronx", "Queens", "Brooklyn", "Staten Island"],
       datasets: [
           {
               label: "The First Dataset",
-              fillColor: "rgba(153,0,76,0.2)", // magenta
-              strokeColor: "rgba(153,0,76,1)", // magenta
-              pointColor: "rgba(153,0,76,1)", // magenta
-              pointStrokeColor: "#fff", // white
-              pointHighlightFill: "#fff", // white
-              pointHighlightStroke: "rgba(153,0,76,1)", // magenta
-              data: [100, 34, 21, 56, 23, 90, 40]
-          },
-          {
-              label: "The Second dataset",
-              fillColor: "rgba(76,0,153,0.2)",
-              strokeColor: "rgba(76,0,153,1)",
-              pointColor: "rgba(76,0,153,1)",
-              pointStrokeColor: "#fff", // white
-              pointHighlightFill: "#fff", // white
-              pointHighlightStroke: "rgba(76,0,153,1)",
-              data: [28, 48, 40, 19, 86, 27, 90]
+              fillColor: "rgba(121, 252, 252,0.2)",
+              strokeColor: "rgba(121, 252, 252,1)",
+              pointColor: "rgba(121, 252, 252,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(121, 252, 252,1)",
+              data: [100, 34, 21, 56, 23]
           }
       ]
   };
 
-  // Step 2. Get the context of the canvas element we want to select
+  var graphInfo = {
+      ///Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines : true,
+      //String - Colour of the grid lines
+      scaleGridLineColor : "rgba(0,0,0,.05)",
+      //Number - Width of the grid lines
+      scaleGridLineWidth : 1,
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines: true,
+      //Boolean - Whether the line is curved between points
+      bezierCurve : true,
+      //Number - Tension of the bezier curve between points
+      bezierCurveTension : 0.4,
+      //Boolean - Whether to show a dot for each point
+      pointDot : true,
+      //Number - Radius of each point dot in pixels
+      pointDotRadius : 4,
+      //Number - Pixel width of point dot stroke
+      pointDotStrokeWidth : 1,
+      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+      pointHitDetectionRadius : 20,
+      //Boolean - Whether to show a stroke for datasets
+      datasetStroke : true,
+      //Number - Pixel width of dataset stroke
+      datasetStrokeWidth : 2,
+      //Boolean - Whether to fill the dataset with a colour
+      datasetFill : true,
+  }
+
+  // Setup graphs
   var ctx = document.getElementById("myChart").getContext("2d");
-  var myNewChart = new Chart(ctx).Line(data, {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-  });
-
-  var ctx2 = document.getElementById("myChart2").getContext("2d");
-  var myNewChart = new Chart(ctx2).Line(data, {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-  });
-
-    var ctx3 = document.getElementById("myChart3").getContext("2d");
-  var myNewChart = new Chart(ctx3).Line(data, {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-  });
-
-    var ctx4 = document.getElementById("myChart4").getContext("2d");
-  var myNewChart = new Chart(ctx4).Line(data, {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-  });
-
-    var ctx5 = document.getElementById("myChart5").getContext("2d");
-  var myNewChart = new Chart(ctx5).Line(data, {
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether to show horizontal lines (except X axis)
-      scaleShowHorizontalLines: true,
-      //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines: true,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-  });
+  var myNewChart = new Chart(ctx).Line(data, graphInfo);
 
 });
